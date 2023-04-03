fc5258d4cb7c5bf0d8fd7a3e5f8c9ad0131bff31
Cedarwud <cedarwudnovels@gmail.com>
2023-04-03 05:57:06 +0000

    Update 8 files

--- portfoliov2-frontend/src/App.js
+++ portfoliov2-frontend/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import Home from './Containers/home'
-import Blog from './Containers/blog'
-import Navmobile from './Components/navmobile'
-import Topnavmobile from './Components/topnavmobile'
-import Blogdetail from './Containers/blogdetail'
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Containers/home";
+import Blog from "./Containers/blog";
+import Navmobile from "./Components/navmobile";
+import Topnavmobile from "./Components/topnavmobile";
+import Blogdetail from "./Containers/blogdetail";
 
 const App = () => {
   return (
     <>
       <Topnavmobile />
-      <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/blog' element={<Blog />} />
-            <Route path='/blog/:id/:slug' element={<Blogdetail />} />
-      </Routes>
-      <div className='md:hidden'><Navmobile /></div>
+      <div className="2xl:container mx-auto" divdiv>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id/:slug" element={<Blogdetail />} />
+        </Routes>
+      </div>
+      <div className="md:hidden">
+        <Navmobile />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- portfoliov2-frontend/src/Containers/home.js
+++ portfoliov2-frontend/src/Containers/home.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import Banner from '../Components/banner'
-import About from '../Components/about'
-import Work from '../Components/work'
-import Technology from '../Components/technology'
-import SEO from '../Components/seo'
+import React from "react";
+import Banner from "../Components/banner";
+import About from "../Components/about";
+import Work from "../Components/work";
+import Technology from "../Components/technology";
+import SEO from "../Components/seo";
 
 const Home = () => {
   return (
-    <div className='bg-bck_color pb-[5rem]'>
-        <SEO 
-          name={"C_edar_"} 
-          title={`Cedar - Web Developer and Graphic Designer`} 
-          description={'Hi, nice to meet you. I am a Freelance Web Developer and Graphic Designer.Let\'s Chat.'} 
-          imgurl={process.env.PUBLIC_URL + '/assets/aboutimg-mobile.png'}
-          imgalt={'A handsome guy smiling ;)'}
-          url={'https://cedar.readate.org'}
-          />
-        <Banner />
-        <Technology />
-        <About />
-        <Work />
+    <div className="bg-bck_color mx-auto pb-[5rem]">
+      <SEO
+        name={"C_edar_"}
+        title={`Cedar - Web Developer and Graphic Designer`}
+        description={
+          "Hi, nice to meet you. I am a Freelance Web Developer and Graphic Designer.Let's Chat."
+        }
+        imgurl={process.env.PUBLIC_URL + "/assets/aboutimg-mobile.png"}
+        imgalt={"A handsome guy smiling ;)"}
+        url={"https://cedar.readate.org"}
+      />
+      <Banner />
+      <Technology />
+      <About />
+      <Work />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
